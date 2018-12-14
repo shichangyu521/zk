71087a95e46665a7f0756e48a40861e06a77640a
石长宇 <shichangyu@izaodao.org>
2018-12-14 15:50:12 +0800

    [zookeeper][master][A][C:石长宇][R:石长宇] 简要描述
    - 1. 启动脚本

--- zj/start.sh
+++ zj/start.sh
@@ -1,22 +1,28 @@
 #!/usr/bin/env bash
 
-#获取镜像名称
-ImageID=`docker images | grep "tomcat_scy" | awk '{print $1}'`
-if [ ! -n ${ImageID} ]; then
-  echo "Dockerfile create image。。。"
-  docker build -t tomcat_scy:1.0.1 /application/dockerfile/tomcat
-fi
-
 #容器名称
-containerName="ziigeerService"
+containerName="zj-web"
 #查询容器id
-ContainersID=`docker ps -a | grep $containerName | awk  '{print $1}'`
-
-if [ -n ${ContainersID} ]; then
-  echo "Container is not NULL，So delete Container 。。。"
-  docker stop $ContainersID
-  docker rm $ContainersID
+ContainersID=`docker ps -a | grep "$containerName" | awk  '{print $1}'`
+#如果容器存在 删除容器
+if [ -n $"ContainersID" ]; then
+    echo "Container is not NULL，So delete Container 。。。"
+    docker stop $ContainersID
+    docker rm $ContainersID
 fi
 
-echo "Create Container 。。。 ContainerId：$containerName"
-docker run --name=$containerName -p 8066:8066 -e TOMCAT_SERVER_ID=tomcat_server_001 -idt tomcat_scy:0.0.7
+#镜像名称
+ImageName="zj-web-image"
+#获取镜像名称
+ImageID=`docker images | grep "$ImageName" | grep "1.0.1" | awk '{print $3}'`
+if [ -n $"ImageID" ]; then
+    #发现镜像 删除镜像
+    echo "Dockerfile Delete image。。。"
+    docker rmi ImageID
+fi
+#dockerfile创建镜像
+docker build -t zj-web:1.0.1 /application/zj-web
+
+#创建容器
+echo "Create Container 。。。 ContainerName: $containerName"
+docker run --name="$containerName" -p 8066:8066 -e TOMCAT_SERVER_ID=tomcat_server_001 -idt "$ImageName":1.0.1
