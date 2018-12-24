ea33447c0a8a1656ee22b9782c1e53884657b666
石长宇 <shichangyu@izaodao.org>
2018-12-24 14:23:21 +0800

    [zookeeper][master][A][C:石长宇][R:石长宇] 简要描述
    - 1. 启动脚本

--- zj/start.sh
+++ zj/start.sh
@@ -13,6 +13,7 @@ fi
 
 #镜像名称
 ImageName="zj-web-image"
+ImageVersion="1.0.1"
 #获取镜像名称
 ImageID=`docker images | grep "$ImageName" | grep "1.0.1" | awk '{print $3}'`
 if [ -n $"ImageID" ]; then
@@ -21,8 +22,8 @@ if [ -n $"ImageID" ]; then
     docker rmi ImageID
 fi
 #dockerfile创建镜像
-docker build -t "$ImageName":1.0.1 /application/zj-web
+docker build -t "$ImageName":"$ImageVersion" /application/zj-web
 
 #创建容器
 echo "Create Container 。。。 ContainerName: $containerName"
-docker run --name="$containerName" -p 8066:8066 -e TOMCAT_SERVER_ID=tomcat_server_001 -idt "$ImageName":1.0.1
+docker run --name="$containerName" -p 8066:8066 -e TOMCAT_SERVER_ID=tomcat_server_001 -idt "$ImageName":"$ImageVersion"
